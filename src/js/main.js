0b7d04649e0ab85faebeeaeebf2c59c8e4140840
Fabian Trinler <fabian.trinler@blateral.com>
2015-06-24 15:50:13 +0200

    Cleaning up main script

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,5 @@
-/****************************************
-    Functions & Helper
-/****************************************/
-(function() {
-    var noop = function noop() {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = window.console || {};
+(function () {
 
-    while (length--) {
-        // Only stub undefined methods.
-        console[methods[length]] = console[methods[length]] || noop;
-    }
-}());
-
-/**
- * A little template parser
- * http://mir.aculo.us/2011/03/09/little-helpers-a-tweet-sized-javascript-templating-engine/
- * Usage: template("Hello {who}!", { who: "JavaScript" });
- */
-function template(s, d) {
-    for(var p in d)
-       s=s.replace(new RegExp('{'+p+'}','g'), d[p]);
-     return s;
-}
-
-(function ($) { 
-    
     // initialize your modules here
-    
-})(jQuery);
 
+})();
