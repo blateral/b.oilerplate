0e9aace0b1d5b3099aee5a490a5d684a5dbc56c6
Alexander Riess <alexander.riess@blateral.com>
2015-09-09 15:10:39 +0200

    Fixing fonts error in build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function(grunt) {
                 'dist/css/*.map',
                 'dist/css/*.css',
                 '!dist/css/*.min.css',
-                '!dist/css/*.fonts.css',
+                '!dist/css/*.woff.css',
                 'dist/js/*.js',
                 '!dist/js/*.min.js',
                 '!dist/**/*.custom.*'
