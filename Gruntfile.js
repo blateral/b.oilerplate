81e847e207246f13ad90dcae0362cd0092e84f15
Fabian Trinler <fabian.trinler@blateral.com>
2016-04-20 15:13:59 +0200

    Getting rid of old assemble task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
 
     // Load all grunt tasks
     require('load-grunt-tasks')(grunt);
-    grunt.loadNpmTasks('assemble');
 
     var settings = grunt.file.readJSON('settings.json');
 
