8919c599429ce6d24837c1d8e45f01bc3f0e8733
Alexander Rieß <alexander.riess@blateral.com>
2017-07-04 16:26:55 +0200

    Adding svgxuse polyfill

--- .blat-scripts/polyfills.js
+++ .blat-scripts/polyfills.js
@@ -1,6 +1,7 @@
 require('whatwg-fetch');
 require('es6-promise/auto');
 require('picturefill');
+require('svgxuse');
 
 Object.assign = require('object-assign');
 Array.from = require('array.from');
